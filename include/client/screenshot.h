aae069081b957dfc71580fa337a3429e4fe02eb9
whiteou7 <fps4day@gmail.com>
2025-11-28 18:22:06 +0700

    feat: add typedef to use byte on linux server

--- include/client/screenshot.h
+++ include/client/screenshot.h
@@ -1,6 +1,11 @@
 #ifndef SCREENSHOT_H
 #define SCREENSHOT_H
-#include <windows.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    typedef unsigned char BYTE;
+#endif
 
 BYTE* SaveScreenshot(size_t* outSize);
 
